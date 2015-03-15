de4f0d7ea3f5c6676ccbfd452681e72fe573768b
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2015-03-16 01:18:22 +0200

    feat: sprinkles of data binding to improve the interactiveness of the documentation

--- tests/dummy/app/routes/options.js
+++ tests/dummy/app/routes/options.js
@@ -4,23 +4,35 @@ export default Ember.Route.extend({
   model: function() {
     return Ember.Object.create({
       defaultRange: { min: 0, max: 100 },
+      defaultStart: [ 20, 80 ],
 
-      startOne: [ 20, 80 ],
-
-      startMargin: [ 20, 80 ],
-
-      limitStart: [ 20, 120 ],
-
-      stepStart: [ 20, 80 ],
-
-      animateStart: 20,
+      margins: [ 20, 80 ],
+      limits: [ 20, 120 ],
+      steps: [ 20, 80 ],
+      direction: 20,
+      animate: 20,
     });
   },
 
   actions: {
+    marginChanged: function(val) {
+      var model = this.modelFor("options");
+      model.set("margins", val);
+    },
+
+    limitsChanged: function(val) {
+      var model = this.modelFor("options");
+      model.set("limits", val);
+    },
+
+    directionChanged: function(val) {
+      var model = this.modelFor("options");
+      model.set("direction", val);
+    },
+
     animateValues: function() {
       var model = this.modelFor("options");
-      model.set('animateStart', 60);
+      model.set('animate', 60);
     }
   }
 });
