e4bace2229d1df1171b19948afcd31cd08e540e1
Paul Chavard <git@paul.chavard.net>
2018-04-06 10:55:08 +0200

    Update ember-cli

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/* jshint node: true */
 'use strict';
 var path = require('path');
 var Funnel = require('broccoli-funnel');
--- testem.js
+++ testem.js
@@ -1,19 +1,24 @@
-/* eslint-env node */
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  launch_in_ci: [
-    'Chrome'
-  ],
-  launch_in_dev: [
-    'Chrome'
-  ],
+  launch_in_ci: ['Chrome', 'Firefox'],
+  launch_in_dev: ['Chrome'],
   browser_args: {
-    Chrome: [
-      '--disable-gpu',
-      '--headless',
-      '--remote-debugging-port=9222',
-      '--window-size=1440,900'
-    ]
+    Firefox: {
+      mode: 'ci',
+      args: ['-headless']
+    },
+    Chrome: {
+      mode: 'ci',
+      args: [
+        // --no-sandbox is needed when running Chrome inside a container
+        process.env.TRAVIS ? '--no-sandbox' : null,
+
+        '--disable-gpu',
+        '--headless',
+        '--remote-debugging-port=0',
+        '--window-size=1440,900'
+      ].filter(Boolean)
+    }
   }
 };
